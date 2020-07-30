fa66fc09b829006e1cb5fb236d49bd5e7e4ffe15
Kristijonas Sendrauskas <edvardas@ivejas.lt>
2020-07-31 00:53:18 +0300

    Iterative rendering

--- app/App.js
+++ app/App.js
@@ -21,8 +21,8 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      mode: "iterative",
-      tree: {
+      mode: "recursive",
+      recursiveTree: {
         name: 'Main node',
         children: [
           { 
@@ -35,47 +35,78 @@ export default class App extends Component {
           { name: 'node', children: [] },
           { name: 'node', children: [] }
         ]
-      }
+      },
+      iterativeTree: [
+        { name: 'Main node', depth: 0 },
+        { name: 'node', depth: 1 },
+        { name: 'node', depth: 2 },
+        { name: 'node', depth: 2 },
+        { name: 'node', depth: 1 },
+        { name: 'node', depth: 1 }
+      ]
     }
   }
 
-  addNode = (depth, path) => {
-    const { tree } = this.state;
-    if (depth === 0) tree.children.push({ name: 'node', children: [] });
-    else pushValue(tree, { name: 'node', children: [] }, path.substring(1, path.length));
-    this.setState({tree});
+  addRecursiveNode = (depth, path) => {
+    const { recursiveTree } = this.state;
+    if (depth === 0) recursiveTree.children.push({ name: 'node', children: [] });
+    else pushValue(recursiveTree, { name: 'node', children: [] }, path.substring(1, path.length));
+    this.setState({recursiveTree});
+  }
+  addIterativeNode = (depth, index) => {
+    const { iterativeTree } = this.state;
+    if (index + 1 === iterativeTree.length) iterativeTree.push({ name: 'node', depth: depth + 1});
+    for (let i = index + 1; i < iterativeTree.length; i++) {
+      if (iterativeTree[i].depth <= depth) {
+        iterativeTree.splice(i, 0, { name: 'node', depth: depth + 1});
+        break;
+      } 
+      else if (i === iterativeTree.length - 1) 
+        iterativeTree.push({ name: 'node', depth: depth + 1});
+    }
+    this.setState({iterativeTree});
   }
 
-  renderNode = (object, depth, path) => {
-    if (this.state.mode === 'iterative')
-      return (
-        <View style={{paddingLeft: 5 * (depth + 1), marginBottom: 5}}>
-          <View style={styles.nameContainer}>
-            <Text>{object.name}</Text>
-            <TouchableOpacity style={styles.button} onPress={this.addNode.bind(this, depth, path)}>
-              <Text style={styles.buttonText}>Add node</Text>
-            </TouchableOpacity>
+  renderRecursiveNode = (object, depth, path) => {
+    return (
+      <View style={{paddingLeft: 5 * (depth + 1), marginBottom: 5}}>
+        <View style={styles.nameContainer}>
+          <Text>{object.name}</Text>
+          <TouchableOpacity style={styles.button} onPress={this.addRecursiveNode.bind(this, depth, path)}>
+            <Text style={styles.buttonText}>Add node</Text>
+          </TouchableOpacity>
+        </View>
+        {object.children.map((child, index) => this.renderRecursiveNode(child, depth + 1, path + `.children.${index}`))}
+      </View>
+    )
+  }
+  iterateTree = (tree) => {
+    return (
+      <View>
+        {tree.map((child, i) => (
+          <View key={i} style={{paddingLeft: 15 * child.depth, marginBottom: 5}}>
+            <View style={styles.nameContainer}>
+              <Text>{child.name}</Text>
+              <TouchableOpacity style={styles.button} onPress={this.addIterativeNode.bind(this, child.depth, i)}>
+                <Text style={styles.buttonText}>Add node</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          {object.children.map((child, index) => this.renderNode(child, depth + 1, path + `.children.${index}`))}
-        </View>
-      )
-    else {
-      return (
-        <View style={{paddingLeft: 5 * (depth + 1), marginBottom: 5}}>
-          
-        </View>
-      )
-    }
+        ))}
+      </View>
+    )
   }
 
-  mapTree = () => <View>{this.renderNode(this.state.tree, 0, '')}</View>
-  switchMode = () => this.setState({mode: this.state.mode === 'iterative' ? 'recursive' : 'iterative'})
-
+  mapTree = () => {
+    if (this.state.mode === 'recursive') return this.renderRecursiveNode(this.state.recursiveTree, 0, '');
+    else return this.iterateTree(this.state.iterativeTree);
+  }
+  switchMode = () => this.setState({mode: this.state.mode === 'iterative' ? 'recursive' : 'iterative'});
   render() {
     return (
       <View style={styles.container}>
         <TouchableOpacity style={styles.modeButton} onPress={this.switchMode}>
-          <Text style={styles.buttonText}>Switch mode</Text>
+          <Text style={styles.buttonText}>{this.state.mode}</Text>
         </TouchableOpacity>
         <ScrollView>
           {this.mapTree()}
